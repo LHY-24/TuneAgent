d84480bd0b1e0fb532b73f0e655554a9779104f7
FlagerLee <46860743+FlagerLee@users.noreply.github.com>
2025-09-03 00:07:11 +0800

    remove unused neo4j config

--- inference/init_env.sh
+++ inference/init_env.sh
@@ -1,8 +1,3 @@
 export OPENAI_BASE_URL=
 export OPENAI_API_KEY=
 export LLM_MODEL=gpt-4o-mini
-
-# neo4j
-export NEO4J_USERNAME=neo4j
-export NEO4J_PASSWORD=12345678
-export NEO4J_URI=neo4j://localhost:7687
